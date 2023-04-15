6dd9eb171fe4bdc83025f65bb53181e4ebb96cff
hoangph.aime <hoangph.aime@gmail.com>
2023-04-15 15:49:47 -0500

    update selenium

--- get_comments.py
+++ get_comments.py
@@ -12,12 +12,11 @@ import os
 
 def extract_email(post_url):
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=chrome_options)
+    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options)
 
     LINKEDIN_USERNAME = os.environ.get('LINKEDIN_USERNAME')
     LINKEDIN_PASSWORD = os.environ.get('LINKEDIN_PASSWORD')
